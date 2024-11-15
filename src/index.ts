5a1dec3b58b13dbf3a13792a8af2fe8c22acdf7d
Yauheni Kryzhyk <krizhik@gmail.com>
2024-11-15 21:24:09 +0300

    default info on main route

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ import { port } from './configs';
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+app.get('/', (req, res) => {
+    res.send("Use '/blogs' or '/posts' route to get data");
+});
