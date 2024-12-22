6523b9509af5379c6fd1c381cc5a70d32661efff
Yauheni Kryzhyk <krizhik@gmail.com>
2024-12-22 18:02:54 +0300

    auth tests

--- __tests__/auth/auth.e2e.test.ts
+++ __tests__/auth/auth.e2e.test.ts
@@ -1,5 +1,9 @@
 import nodemailer from 'nodemailer';
-import { baseRoutes } from '../../src/app/configs';
+import {
+    accessTokenExpirationTime,
+    baseRoutes,
+    refreshTokenExpirationTime,
+} from '../../src/app/configs';
 import {
     addNewUser,
     DBHandlers,
@@ -15,6 +19,11 @@ import { usersRepository } from '../../src/domain/users';
 import { routersPaths } from '../../src/app/configs';
 
 jest.mock('nodemailer');
+jest.mock('../../src/app/configs', () => ({
+    ...jest.requireActual('../../src/app/configs'),
+    accessTokenExpirationTime: 1,
+    refreshTokenExpirationTime: 2,
+}));
 
 describe('Auth Controller', () => {
     const testUser = getTestUser(1);
@@ -80,6 +89,44 @@ describe('Auth Controller', () => {
         });
     });
 
+    describe('POST /logout', () => {
+        let sessionCookie: string;
+
+        beforeEach(async () => {
+            const response = await req
+                .post(`${baseRoutes.auth}${routersPaths.auth.login}`)
+                .send(loginCredentials);
+            sessionCookie = response.get('set-cookie') || '';
+        });
+
+        it('returns an error if session token is expired', async () => {
+            await new Promise((resolve) =>
+                setTimeout(resolve, refreshTokenExpirationTime * 1000)
+            );
+            await req
+                .post(`${baseRoutes.auth}${routersPaths.auth.logout}`)
+                .set('Cookie', sessionCookie)
+                .expect(HTTP_STATUS_CODES.UNAUTHORIZED);
+        });
+
+        it('returns an error if no cookie passed', async () => {
+            await req
+                .post(`${baseRoutes.auth}${routersPaths.auth.logout}`)
+                .expect(HTTP_STATUS_CODES.UNAUTHORIZED);
+        });
+
+        it('logout successfully and return an error trying to update tokens with the same refreshToken', async () => {
+            await req
+                .post(`${baseRoutes.auth}${routersPaths.auth.logout}`)
+                .set('Cookie', sessionCookie)
+                .expect(HTTP_STATUS_CODES.NO_CONTENT);
+            await req
+                .post(`${baseRoutes.auth}${routersPaths.auth.refreshToken}`)
+                .set('Cookie', sessionCookie)
+                .expect(HTTP_STATUS_CODES.UNAUTHORIZED);
+        });
+    });
+
     describe('GET /me', () => {
         let accessToken = '';
         beforeAll(async () => {
@@ -115,6 +162,68 @@ describe('Auth Controller', () => {
             expect(response.body.userId).toBeDefined();
             expect(response.body.email).toBe(testUser.email);
             expect(response.body.login).toBe(testUser.login);
+        });
+
+        it('returns an error if access token is expired', async () => {
+            await new Promise((resolve) =>
+                setTimeout(resolve, accessTokenExpirationTime * 1000)
+            );
+            const response = await req
+                .get(`${baseRoutes.auth}${routersPaths.auth.me}`)
+                .auth(accessToken, { type: 'bearer' });
+            expect(response.status).toBe(HTTP_STATUS_CODES.UNAUTHORIZED);
+        });
+    });
+
+    describe('POST /refresh-token', () => {
+        let accessToken = '';
+        let sessionCookie = '';
+        let updatedSessionCookie = '';
+
+        beforeAll(async () => {
+            const loginCredentials = {
+                loginOrEmail: testUser.login,
+                password: testUser.password,
+            };
+            const response = await req
+                .post(`${baseRoutes.auth}${routersPaths.auth.login}`)
+                .send(loginCredentials);
+            accessToken = response.body.accessToken;
+            sessionCookie = response.get('set-cookie') || '';
+        });
+
+        it('updates tokens successfully', async () => {
+            //need to wait for generating different tokens from login in beforeAll
+            await new Promise((resolve) =>
+                setTimeout(resolve, accessTokenExpirationTime * 1000)
+            );
+            const response = await req
+                .post(`${baseRoutes.auth}${routersPaths.auth.refreshToken}`)
+                .set('Cookie', sessionCookie)
+                .expect(HTTP_STATUS_CODES.OK);
+            expect(response.body.accessToken).toBeDefined();
+            expect(response.body.accessToken).not.toEqual(accessToken);
+            expect(response.headers['set-cookie']).toBeDefined();
+            expect(response.headers['set-cookie']).not.toEqual(sessionCookie);
+
+            updatedSessionCookie = response.get('set-cookie') || '';
+        });
+
+        it('return an error while using revoked refresh token', async () => {
+            await req
+                .post(`${baseRoutes.auth}${routersPaths.auth.refreshToken}`)
+                .set('Cookie', sessionCookie)
+                .expect(HTTP_STATUS_CODES.UNAUTHORIZED);
+        });
+
+        it('return an error if refresh token is expired', async () => {
+            await new Promise((resolve) =>
+                setTimeout(resolve, (refreshTokenExpirationTime + 1) * 1000)
+            );
+            await req
+                .post(`${baseRoutes.auth}${routersPaths.auth.refreshToken}`)
+                .set('Cookie', updatedSessionCookie)
+                .expect(HTTP_STATUS_CODES.UNAUTHORIZED);
         });
     });
 
